a4df19db4896a0fdaf643590b411448561faeaf5
your friendly trustworthy programmer <117580169+RealSput@users.noreply.github.com>
2023-10-10 20:31:41 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,63 +1,83 @@
-/*
-let's say X is 15 and Y is 15, and our width is 10 and length is 10
+require('@g-js-api/g.js');
 
-we add a block at (15, 15), then subtract the Y by the width, and we get 5, so we place another block at (10, 5)
-then we add the length to X, so we place another block at (25, 10) and place a final block at (25, 5) by doing both operations
+const Jimp = require('jimp');
+let { Bitmap, ImageRunner, ShapeTypes, ShapeJsonExporter } = require('geometrizejs');
 
-however, we should fill in the blocks using ranges (e.g. Y = 15..5, X = 10..25)
-*/
+function rgb2hsv (r, g, b) {
+    let rabs, gabs, babs, rr, gg, bb, h, s, v, diff, diffc, percentRoundFn;
+    rabs = r / 255;
+    gabs = g / 255;
+    babs = b / 255;
+    v = Math.max(rabs, gabs, babs),
+    diff = v - Math.min(rabs, gabs, babs);
+    diffc = c => (v - c) / 6 / diff + 1 / 2;
+    percentRoundFn = num => Math.round(num * 100) / 100;
+    if (diff == 0) {
+        h = s = 0;
+    } else {
+        s = diff / v;
+        rr = diffc(rabs);
+        gg = diffc(gabs);
+        bb = diffc(babs);
 
-/*
-require('@g-js-api/g.js')
-
-let [x, y] = [15, 15];
-let [width, length] = [10, 10]
-let id = 1;
-
-let block = (x, y, width, length) => {
-	let range_x = ' '.repeat(x + length).split(' ').map((_, i) => (30 * i) + x)
-	
-	let range_y = ' '.repeat(y - width).split(' ').map((_, i) => (30 * i) + y);
-	
-	range_x.forEach(cx => {
-		console.log(cx)
-		range_y.forEach(cy => {
-			$.add({
-				OBJ_ID: id,
-				X: cx,
-				Y: cy
-			})
-		});
-	});
+        if (rabs === v) {
+            h = bb - gg;
+        } else if (gabs === v) {
+            h = (1 / 3) + rr - bb;
+        } else if (babs === v) {
+            h = (2 / 3) + gg - rr;
+        }
+        if (h < 0) {
+            h += 1;
+        }else if (h > 1) {
+            h -= 1;
+        }
+    }
+    let [hue, saturation, brightness] = [Math.round(h * 360), percentRoundFn(s * 100) / 100, percentRoundFn(v * 100) / 100]
+	return `${hue}a${saturation}a${brightness}a0a0`;
 }
 
-block(15, 15, 10, 10);
+let col = unknown_c();
+col.set(rgb(255, 0, 0));
 
-$.exportToSavefile();
-*/
+let zo = 0;
 
+let circle = (x, y, radius, rgba) => {	
+	let str = rgb2hsv(...rgba.slice(0, -1));
 
-require('@g-js-api/g.js')
-
-let [x, y] = [15, 15];
-let [width, length] = [10, 10]
-let id = 1;
-
-let block = (x, y, width, length) => {
-	let range_x = range(x, x + length).map(x => x * 3);
-	let range_y = range(y, y - width).map(x => x * 3);
-	
-	range_x.forEach(cx => {
-		range_y.forEach(cy => {
-			$.add({
-				OBJ_ID: id,
-				X: cx,
-				Y: cy
-			})
-		});
+	$.add({
+		OBJ_ID: 1764,
+		X: x,
+		Y: y,
+		SCALING: radius / 4,
+		HVS_ENABLED: 1,
+		HVS: str,
+		COLOR: col,
+		Z_ORDER: zo
 	});
+	
+	zo++;
 }
 
-block(15, 15, 10, 10);
-
-$.exportToSavefile();
+(async () => {
+  let image = await Jimp.read('art.png')
+  image = image.flip(false, true);
+  const bitmap = Bitmap.createFromByteArray(image.bitmap.width, 
+    image.bitmap.height, image.bitmap.data)
+  const runner = new ImageRunner(bitmap)
+  const options = {
+    shapeTypes: [ShapeTypes.CIRCLE],
+    candidateShapesPerStep: 50,
+    shapeMutationsPerStep: 100,
+    alpha: 255
+  }
+  const iterations = 200
+  const shapes = []
+  for (let i = 0; i < iterations;i++) {
+	   // data: 0 = x, 1 = y, 2 = width, 3 = height
+	   let x = JSON.parse(ShapeJsonExporter.exportShapes(runner.step(options)));
+	   circle(...x.data, x.color);
+  }
+  
+  $.exportToSavefile({ info: true });
+})()
